3a2186b30d594ea083caa4cdcbc17a366a05675e
ZZIDZZ <zidanutomo@gmail.com>
2022-02-14 18:01:51 +0700

    added samples and resize fix

--- resize-set.py
+++ resize-set.py
@@ -5,17 +5,21 @@ import os
 print(os.getcwd())
 os.chdir("./dataset")
 
-test_image = "0.png"
-original = Image.open(test_image)
+def resize(image):
+    original = Image.open(image)
+    # Get dimensions
+    width, height = original.size
+    left = width/2.5
+    top = height/3
+    right = 3 * width/5
+    bottom = 2 * height/3
+    return original.crop((left, top, right, bottom))
 
-# Get dimensions
-width, height = original.size
-left = width/4
-top = height/4
-right = 3 * width/4
-bottom = 3 * height/4
-cropped_example = original.crop((left, top, right, bottom))
-
-outname = f"{test_image[:-4]}a.png"
-cropped_example.save(outname)
-print(outname)
+for f in os.listdir():
+    if "r" in f:
+        continue
+    elif os.path.isfile(f):
+        resized = resize(f)
+        outname = f"{f[:-4]}r.png"
+        resized.save(outname)
+        print(f)
